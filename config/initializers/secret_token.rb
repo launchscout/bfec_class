dbab5c933f67701dde549c71409c52ff451fcdb3
Doug Alcorn <dougalcorn@gmail.com>
2011-10-30 15:48:43 -0400

    clean rails 3.1.1 cookbook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '856c074fc36fe7374bb57429ebf1b6da7d1c980656b02a814093eaf126e855fcccd29b296e646e72b179c22b339b4e606dd161906b49957db14f9190238b2e12'
